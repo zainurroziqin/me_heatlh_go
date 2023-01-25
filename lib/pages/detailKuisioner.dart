7b45b16055dc757cdae33e442cd0afabd9a26244
Zril <74762223+azrilnr@users.noreply.github.com>
2023-01-25 21:37:29 +0700

    Update Kuisoner
    Button Submit

--- lib/pages/detailKuisioner.dart
+++ lib/pages/detailKuisioner.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
 import 'package:me_heatlh_go/config/theme.dart';
+import 'package:me_heatlh_go/pages/home.dart';
+import 'package:me_heatlh_go/pages/home/artikelPage.dart';
+import 'package:me_heatlh_go/pages/home/contactPage.dart';
 
 class DetailKuisioner extends StatefulWidget {
   const DetailKuisioner({Key? key}) : super(key: key);
@@ -15,9 +19,65 @@ class _DetailKuisionerState extends State<DetailKuisioner> {
     return Scaffold(
       backgroundColor: primariColor,
       body: Padding(
-        padding: EdgeInsets.only(top: 70.h, bottom: 50.h),
+        padding: EdgeInsets.symmetric(horizontal: 15.w, vertical: 10.h),
         child: Center(
-          child: Text('Detail Kuisioner'),
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            height: 150.h,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              color: whiteColor,
+            ),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 15.h,
+                ),
+                Text(
+                  'Hasil dari pengisian Kuisioner adalah :',
+                ),
+                SizedBox(
+                  height: 15.h,
+                ),
+                Text(
+                  '=== Mengalami Kecemasan ===',
+                ),
+                SizedBox(
+                  height: 30.h,
+                ),
+                GestureDetector(
+                  onTap: () {
+                    Get.back();
+                  },
+                  child: Container(
+                    width: 200.w,
+                    height: 40.h,
+                    decoration: BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                          color: lightColor.withOpacity(0.3),
+                          spreadRadius: 0,
+                          blurRadius: 4,
+                          offset: const Offset(0, 4),
+                        ),
+                      ],
+                      color: secondaryColor,
+                      borderRadius: BorderRadius.circular(48.w),
+                    ),
+                    child: Center(
+                      child: Text(
+                        'KEMBALI',
+                        style: sarala40016.copyWith(
+                            fontSize: 12.sp,
+                            color: blackColor,
+                            fontWeight: FontWeight.w700),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
