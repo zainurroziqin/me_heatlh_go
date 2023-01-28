1d41f41e583f04e92ed3e784ac643b65377a91d1
zainurroziqin <75110898+zainurroziqin@users.noreply.github.com>
2023-01-28 12:14:07 +0700

    update overflow login page

--- lib/pages/loginPage.dart
+++ lib/pages/loginPage.dart
@@ -48,174 +48,181 @@ class _LoginPageState extends State<LoginPage> {
     }
 
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       backgroundColor: primariColor,
       body: SafeArea(
-          child: Stack(
-        children: [
-          Form(
-            key: formKey,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+          child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Stack(
               children: [
-                SizedBox(
-                  height: 64.h,
-                ),
-                Align(
-                    alignment: Alignment.topCenter,
-                    child: Image.asset('assets/logo.png')),
-                SizedBox(
-                  height: 32.h,
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 32.w),
+                Form(
+                  key: formKey,
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Text(
-                        'USERNAME',
-                        style: sarala40016,
-                      ),
-                      SizedBox(
-                        height: 6.h,
-                      ),
-                      Container(
-                        width: 320.w,
-                        height: 48.h,
-                        decoration: BoxDecoration(
-                            color: whiteColor,
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(24.w))),
-                        child: TextFormField(
-                          controller: emailController,
-                          decoration: InputDecoration(
-                            hintText: 'Masukkan Username',
-                            hintStyle: styleHintText,
-                            border: OutlineInputBorder(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(24.w)),
-                            ),
-                          ),
-                        ),
-                      ),
-                      SizedBox(
-                        height: 18.h,
-                      ),
-                      Text(
-                        'PASSWORD',
-                        style: sarala40016,
-                      ),
-                      SizedBox(
-                        height: 6.h,
-                      ),
-                      Container(
-                        width: 320.w,
-                        height: 48.h,
-                        decoration: BoxDecoration(
-                            color: whiteColor,
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(24.w))),
-                        child: TextFormField(
-                          controller: passwordController,
-                          obscureText: showPassword,
-                          decoration: InputDecoration(
-                            suffixIcon: GestureDetector(
-                                onTap: () {
-                                  if (showPassword) {
-                                    setState(() {
-                                      showPassword = false;
-                                    });
-                                  } else {
-                                    setState(() {
-                                      showPassword = true;
-                                    });
-                                  }
-                                },
-                                child: showPassword
-                                    ? const Icon(Icons.visibility)
-                                    : const Icon(Icons.visibility_off)),
-                            hintText: 'Masukkan Password',
-                            hintStyle: styleHintText,
-                            border: OutlineInputBorder(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(24.w)),
-                            ),
-                          ),
-                        ),
-                      ),
                       SizedBox(
                         height: 64.h,
                       ),
-                      GestureDetector(
-                        onTap: () {
-                          login();
-                        },
-                        child: Container(
-                          width: 320.w,
-                          height: 48.h,
-                          decoration: BoxDecoration(
-                            boxShadow: [
-                              BoxShadow(
-                                color: lightColor.withOpacity(0.3),
-                                spreadRadius: 0,
-                                blurRadius: 4,
-                                offset: const Offset(0, 4),
-                              ),
-                            ],
-                            color: secondaryColor,
-                            borderRadius: BorderRadius.circular(48.w),
-                          ),
-                          child: const Center(
-                            child: Text(
-                              'LOGIN',
-                              style: TextStyle(
-                                fontWeight: FontWeight.w500,
-                                color: Colors.black,
-                                letterSpacing: 2,
+                      Align(
+                          alignment: Alignment.topCenter,
+                          child: Image.asset('assets/logo.png')),
+                      SizedBox(
+                        height: 32.h,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 32.w),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'USERNAME',
+                              style: sarala40016,
+                            ),
+                            SizedBox(
+                              height: 6.h,
+                            ),
+                            Container(
+                              width: 320.w,
+                              height: 48.h,
+                              decoration: BoxDecoration(
+                                  color: whiteColor,
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(24.w))),
+                              child: TextFormField(
+                                controller: emailController,
+                                decoration: InputDecoration(
+                                  hintText: 'Masukkan Username',
+                                  hintStyle: styleHintText,
+                                  border: OutlineInputBorder(
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(24.w)),
+                                  ),
+                                ),
                               ),
                             ),
-                          ),
+                            SizedBox(
+                              height: 18.h,
+                            ),
+                            Text(
+                              'PASSWORD',
+                              style: sarala40016,
+                            ),
+                            SizedBox(
+                              height: 6.h,
+                            ),
+                            Container(
+                              width: 320.w,
+                              height: 48.h,
+                              decoration: BoxDecoration(
+                                  color: whiteColor,
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(24.w))),
+                              child: TextFormField(
+                                controller: passwordController,
+                                obscureText: showPassword,
+                                decoration: InputDecoration(
+                                  suffixIcon: GestureDetector(
+                                      onTap: () {
+                                        if (showPassword) {
+                                          setState(() {
+                                            showPassword = false;
+                                          });
+                                        } else {
+                                          setState(() {
+                                            showPassword = true;
+                                          });
+                                        }
+                                      },
+                                      child: showPassword
+                                          ? const Icon(Icons.visibility)
+                                          : const Icon(Icons.visibility_off)),
+                                  hintText: 'Masukkan Password',
+                                  hintStyle: styleHintText,
+                                  border: OutlineInputBorder(
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(24.w)),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            SizedBox(
+                              height: 64.h,
+                            ),
+                            GestureDetector(
+                              onTap: () {
+                                login();
+                              },
+                              child: Container(
+                                width: 320.w,
+                                height: 48.h,
+                                decoration: BoxDecoration(
+                                  boxShadow: [
+                                    BoxShadow(
+                                      color: lightColor.withOpacity(0.3),
+                                      spreadRadius: 0,
+                                      blurRadius: 4,
+                                      offset: const Offset(0, 4),
+                                    ),
+                                  ],
+                                  color: secondaryColor,
+                                  borderRadius: BorderRadius.circular(48.w),
+                                ),
+                                child: const Center(
+                                  child: Text(
+                                    'LOGIN',
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.w500,
+                                      color: Colors.black,
+                                      letterSpacing: 2,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            SizedBox(
+                              height: 24.h,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                const Text(
+                                  'Belum punya akun? ',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.w400,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                InkWell(
+                                  onTap: () {
+                                    Get.to(RegisterPage());
+                                  },
+                                  child: const Text(
+                                    'Register',
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.w500,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            )
+                          ],
                         ),
                       ),
-                      SizedBox(
-                        height: 24.h,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          const Text(
-                            'Belum punya akun? ',
-                            style: TextStyle(
-                              fontWeight: FontWeight.w400,
-                              color: Colors.white,
-                            ),
-                          ),
-                          InkWell(
-                            onTap: () {
-                              Get.to(RegisterPage());
-                            },
-                            child: const Text(
-                              'Register',
-                              style: TextStyle(
-                                fontWeight: FontWeight.w500,
-                                color: Colors.white,
-                              ),
-                            ),
-                          ),
-                        ],
-                      )
                     ],
                   ),
                 ),
+                Obx(() {
+                  if (cLogin.loading) {
+                    return showCustomLoadingDialog(context);
+                  }
+                  return const SizedBox.shrink();
+                }),
               ],
             ),
-          ),
-          Obx(() {
-            if (cLogin.loading) {
-              return showCustomLoadingDialog(context);
-            }
-            return const SizedBox.shrink();
-          }),
-        ],
+          ],
+        ),
       )),
     );
   }
